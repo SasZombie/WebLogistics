6aab79e35ed5f0ead74307146a695f43d1653863
SasZombie <gigelusas81@gmail.com>
2025-04-02 16:58:28 +0300

    Fixed animation on Firefox :D

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,7 @@
             <h2 class="text-xl font-semibold mb-4">Books</h2>
             <div v-for="book in booksOutput" :key="book.title" class="mb-2">
                 <h3 @click="goToDetails(book)" class="cursor-pointer text-lg hover:underline p-2" :class="{
-                    'border-2 rounded-md': isBookRecomanded(book),
-                    'circular-gradient-border': isBookRecomanded(book) // Apply the smoother circular moving gradient
+                    'circular-gradient-border': isBookRecomanded(book) 
                 }">
                     {{ book.title }}
                 </h3>
@@ -31,13 +30,94 @@
     </div>
 </template>
 
+<!-- 8 -->
 <style scoped>
 @keyframes gradientRotation {
     0% {
-        border-image-source: linear-gradient(45deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+        border-image-source: linear-gradient(18deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    5% {
+        border-image-source: linear-gradient(36deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    10% {
+        border-image-source: linear-gradient(54deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    15% {
+        border-image-source: linear-gradient(72deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    20% {
+        border-image-source: linear-gradient(90deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
     }
 
     25% {
+        border-image-source: linear-gradient(108deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    30% {
+        border-image-source: linear-gradient(126deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    35% {
+        border-image-source: linear-gradient(144deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    40% {
+        border-image-source: linear-gradient(162deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    45% {
+        border-image-source: linear-gradient(180deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    50% {
+        border-image-source: linear-gradient(198deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    55% {
+        border-image-source: linear-gradient(216deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    60% {
+        border-image-source: linear-gradient(234deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    65% {
+        border-image-source: linear-gradient(252deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    70% {
+        border-image-source: linear-gradient(270deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    75% {
+        border-image-source: linear-gradient(288deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    80% {
+        border-image-source: linear-gradient(306deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    85% {
+        border-image-source: linear-gradient(324deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    90% {
+        border-image-source: linear-gradient(342deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    95% {
+        border-image-source: linear-gradient(360deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    100% {
+        border-image-source: linear-gradient(0deg, pink, rgb(212, 35, 65), rgb(201, 10, 159));
+    }
+
+    /* 25% {
         border-image-source: linear-gradient(135deg, pink,  rgb(212, 35, 65), rgb(201, 10, 159));
     }
 
@@ -51,14 +131,14 @@
 
     100% {
         border-image-source: linear-gradient(45deg, pink,  rgb(212, 35, 65), rgb(201, 10, 159));
-    }
+    } */
 }
 
 .circular-gradient-border {
     border: 4px solid transparent;
     border-radius: 8px;
     border-image: linear-gradient(45deg, red, yellow, green) 1;
-    animation: gradientRotation 3s infinite linear;
+    animation: gradientRotation 2s infinite linear;
 }
 </style>
 
